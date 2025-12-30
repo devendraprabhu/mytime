6a6ccbce53cc54e77379ccd8745d65d54631b49b
Devendra Prabhu <devendraprabhu05@gmail.com>
2025-12-30 23:12:23 +0530

    version2 People

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const app=require('mongoose')
  const conn=app.connect('mongodb://localhost/mytime').then(()=>{
     console.log('database connected');
+    console.log("I Changed this")
  })
 
 module.exports=app

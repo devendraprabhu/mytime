5d3ba027768b15cf1abf3d0981fa9abb8de0cf90
Devendra Prabhu <devendraprabhu05@gmail.com>
2025-12-30 23:30:06 +0530

    HEllo

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const app=require('mongoose')
  const conn=app.connect('mongodb://localhost/mytime').then(()=>{
     console.log('database connected');
-    console.log("I Changed this")
+    console.log("I Changed this People")
  })
 
 module.exports=app
